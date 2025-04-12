93b2a5c7600eeffa3bbdbc7dd863ecba568bbe7b
Vlad Belousov <vladbelousov@Vlads-MBP.lan>
2025-04-12 18:57:02 +0300

    bla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import InvestmentsPreview from "./components/InvestmentsPreview";
 import RenovationsPreview from "./components/RenovationsPreview";
 import TeamPreview from "./components/TeamPreview";
 import PropertyManagement from "./components/PropertyManagment"
-import Hero from "./components/hero"
-import ContactCTA from "./components/ContactCTA";
 import WhyInvest  from "./components/whybudapest";
 export default function HomePage() {
   return (
